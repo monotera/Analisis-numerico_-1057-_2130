ab617c7d197fefeedc897c9983b2a862f1659a05
Nelson Mosquera <nelson.a.mosquera@gmail.com>
2021-07-23 19:31:03 -0500

    Resolves minor bug

--- Primeros_Problemas/PrimerProblema.py
+++ Primeros_Problemas/PrimerProblema.py
@@ -2,7 +2,7 @@
 import math
 
 def sqrt(n, E, x):
-    if(x < 0 or n < 0):
+    if(x <= 0 or n < 0):
         raise ValueError("El valor de n y/o x no puede ser negativo")
     elif n == 0:
         return 0
@@ -13,6 +13,6 @@ def sqrt(n, E, x):
     return y
 
 
-print("Raiz cuadrarda de 7 con una tolerancia de 10^-8 ", sqrt(7, pow(10, -8), 2))
-print("Raiz cuadrarda de 7 con una tolerancia de 10^-16 ", sqrt(7, pow(10, -16), 2))
+print("Raiz cuadrarda de 7 con una tolerancia de 10^-8 ", sqrt(7, pow(10, -8), 1))
+print("Raiz cuadrarda de 7 con una tolerancia de 10^-16 ", sqrt(7, pow(10, -16), 1))
 print("Raiz cuadrarda de 7 utilizando funcion de python sqrt ",  math.sqrt(7))
